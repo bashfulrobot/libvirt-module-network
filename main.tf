5edf40a34336311a5a89bc6938444ce9e335b3d2
bashfulrobot <dustin@bashfulrobot.com>
2024-06-22 23:18:49 -0700

    feat: Refactor network configuration
    - Delete `terraform.tf` file
    - Add network module to `main.tf`
    - Add variables for k8s cluster network
    - Add resources and outputs for libvirt network
    - Delete `variables.tf` file and remove all variables
    - Delete `output.tf` file and remove output blocks
    
    [terraform.tf]
    - Delete `terraform.tf` file which was no longer needed.
    [main.tf]
    - Added a network module to the `main.tf` file
    - Added variables for the k8s cluster network
    - Added resources for the libvirt network
    - Added outputs for the libvirt network
    [variables.tf]
    - The `variables.tf` file was deleted in this commit.
    - The `kvm_subnet` variable was removed.
    - The `network_name` variable was removed.
    - The `dhcp_enabled` variable was removed.
    - The `dns_enabled` variable was removed.
    - The `dns_local_only` variable was removed.
    - The `autostart_network` variable was removed.
    - The `network_mode` variable was removed.
    [output.tf]
    - Delete the file `output.tf`
    - Remove the `output "network_id"` block
    - Remove the `output "network_domain"` block

--- main.tf
+++ main.tf
@@ -1,3 +1,60 @@
+###### NETWORK MODULE ######
+terraform {
+  #   required_version = ">= 1.0.1"
+  required_providers {
+    # see https://registry.terraform.io/providers/dmacvicar/libvirt
+    # see https://github.com/dmacvicar/terraform-provider-libvirt
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = ">= 0.7.6"
+    }
+  }
+}
+
+##### VARIABLES #####
+variable "kvm_subnet" {
+  type    = string
+  default = "172.16.0.0/24"
+}
+
+variable "network_name" {
+  description = "A name to provide for the k8s cluster network"
+  type        = string
+  default     = "deault"
+}
+
+variable "dhcp_enabled" {
+  description = "Enable DHCP for the k8s cluster network"
+  type        = bool
+  default     = false
+}
+
+variable "dns_enabled" {
+  description = "Enable DNS for the k8s cluster network"
+  type        = bool
+  default     = true
+}
+
+variable "dns_local_only" {
+  description = "Enable local only DNS for the k8s cluster network"
+  type        = bool
+  default     = false
+}
+
+variable "autostart_network" {
+  description = "Autostart the k8s cluster network"
+  type        = bool
+  default     = true
+}
+
+variable "network_mode" {
+  description = "The network mode for the k8s cluster network"
+  type        = string
+  default     = "nat"
+}
+
+##### RESOURCES #####
+
 # see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.7.1/website/docs/r/network.markdown
 resource "libvirt_network" "cluster_net" {
   name      = var.network_name
@@ -12,4 +69,15 @@ resource "libvirt_network" "cluster_net" {
     enabled    = var.dns_enabled
     local_only = var.dns_local_only
   }
+}
+
+##### OUTPUTS #####
+output "network_id" {
+  description = "ID of libvirt network"
+  value       = libvirt_network.cluster_net.id
+}
+
+output "network_domain" {
+  description = "Domain of libvirt network"
+  value       = libvirt_network.cluster_net.domain
 }
--- output.tf
+++
@@ -1,9 +0,0 @@
-output "network_id" {
-  description = "ID of libvirt network"
-  value       = libvirt_network.cluster_net.id
-}
-
-output "network_domain" {
-  description = "Domain of libvirt network"
-  value       = libvirt_network.cluster_net.domain
-}
